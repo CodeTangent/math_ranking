5812d940eea375ef7447fe958524093f75bbcc4d
renan <rgcdl47@gmail.com>
2026-03-07 01:09:28 -0300

    Alterado o nome do arquivo "steps.txt" para "Keys.txt" onde esta a api  do banco de dados. criado o arquivo usuario_teste.py para que se tenha um usuário para testar o login. #Para consumir o usuário teste utilize GET

---
+++ usuario_teste.py
@@ -0,0 +1,27 @@
+# Esse usuário é exlusivamente para testes iniciais de login
+# Usar parâmetro de requisição para testes: GET
+# Se quiser testar com POST usar linha comentada que deixei no código
+
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+credenciais_do_usuario = [
+    {"email": "user", "password": "123"}
+]
+
+#@app.route("/login" methods=["POST"])
+@app.route("/login")
+def login():
+    dados_do_usuario = request.json
+
+    if (
+        dados_do_usuario.get["email"] == credenciais_do_usuario["email"] 
+        and dados_do_usuario.get["password"] == credenciais_do_usuario["password"]
+        
+    ):
+        return jsonify(True)
+    return jsonify(False)
+
+app.run()
