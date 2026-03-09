5f1f400e4910e932741d2c3724cfd92681469e94
Renan Lima <126360827+Renan-G-Lima@users.noreply.github.com>
2026-03-09 17:57:38 -0300

    Corrigindo falta de implantação do CORS
    Corrigi a falta da importação do flask cors que permite que a api seja requisitada do frontend sem ser bloqueada pelo flask

--- usuario_teste.py
+++ usuario_teste.py
@@ -4,8 +4,10 @@
 
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 credenciais_do_usuario = [
     {"email": "user", "password": "123"}
