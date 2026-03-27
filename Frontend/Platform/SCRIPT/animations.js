74e4d244df4014ca8e426814d6441e7f1d90dd4d
Justjuaum <joao.vo222@gmail.com>
2026-03-26 21:40:35 -0300

    Implementação do login junto das API de mock
    Login funcional com redirecionamento para página de home.
    Observar que os erros anteriores ainda continuam os mesmos, falta responsividade principalmente no layout da box que faz o switch entre login e registro.

---
+++ Frontend/Platform/SCRIPT/animations.js
@@ -0,0 +1,54 @@
+//Estilizacao
+function alternateEye(){
+    const pass = document.getElementById("password");
+    const eye = document.getElementById("btn-senha");
+    
+    if(pass.type === 'password'){
+        pass.setAttribute("type", "text");
+        eye.classList.replace('bi-eye', 'bi-eye-slash');
+    }else{
+        pass.setAttribute("type", "password");
+        eye.classList.replace('bi-eye-slash', 'bi-eye');
+    }
+}
+
+function toggleAnimation(type){
+    const box = document.getElementById("Box");
+
+    if(type === "login"){
+        box.animate([
+            {
+                transform: "translateX(0px)", 
+                borderRadius: "0 2em 2em 0em",
+            },
+            {
+                transform: "translateX(-400px)", 
+                borderRadius: "2em 0 0 2em",
+            }
+        ], {
+            duration: 800,
+            fill: "forwards",
+            easing: "ease-in-out"
+        });
+    }else if(type === "register"){
+        box.animate([
+            {
+                transform: "translateX(-400px)", 
+                borderRadius: "2em 0 0 2em",
+            },
+            {
+                transform: "translateX(0px)", 
+                borderRadius: "0 2em 2em 0em",
+            }
+        ], {
+            duration: 800,
+            fill: "forwards",
+            easing: "ease-in-out"
+        });
+    }
+}
+
+document.querySelector('._form_login').addEventListener('submit', function(event) {
+    event.preventDefault();
+    login();
+});
---
+++ Frontend/Platform/SCRIPT/login_and_register.js
@@ -0,0 +1,58 @@
+import {connection_link} from './connection_link.js';
+
+const CL = new connection_link();
+const url = CL.getUrl();
+
+async function login(){
+    const _email = document.getElementById("email").value;
+    const _password = document.getElementById("password").value;
+
+    const dataLogin = {email: _email, password: _password}
+    try {
+        const response = await fetch(url,{
+            method: 'POST',
+            headers: {'Content-type' : 'application/json',}, 
+            body: JSON.stringify(dataLogin),
+        })
+
+        const data = await response.json();
+
+        if(response.ok && data){
+            return window.location.href= 'home.html';
+        }
+        alert("Usuário incorreto"); 
+    }
+    catch(e){
+        console.log("Error: ", e.message);
+    }
+
+}
+
+async function register(){
+    const _email = document.getElementById(emailR).value;
+    const _password = document.getElementById(passwordR).value;
+    const _confirmation = document.getElementById(passwordC).value;
+
+    const dataRegister = {email: _email, password: _password, confirmation: _confirmation}
+    try {
+        const response = await fetch(url,{
+            method: 'POST',
+            headers: {'Content-type' : 'application/json',}, 
+            body: JSON.stringify({dataRegister}),
+        })
+
+        if(response.ok){
+            const data = await response.json();
+            localStorage.setItem('acess_token', response.token);
+            window.location.href = "home.html";
+        }
+    }
+    catch(e){
+        console.log("Error: ", e.message);
+    }
+}
+
+document.querySelector('._form_login').addEventListener('submit', function(event) {
+    event.preventDefault();
+    login();
+});
